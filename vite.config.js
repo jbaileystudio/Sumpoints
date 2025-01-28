d791a6ab9d4046860fcaf16dfa6d630bb014544e
jbaileystudio <90975886+jbaileystudio@users.noreply.github.com>
2025-01-28 09:25:31 -0500

    Fix base path capitalization

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sumpoints/',
+  base: '/Sumpoints/',
   build: {
     outDir: 'dist',
     rollupOptions: {
