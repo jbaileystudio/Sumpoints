e77b84612f19019f5a9e2a7666f5a7bed6c9151a
jbaileystudio <90975886+jbaileystudio@users.noreply.github.com>
2025-01-28 07:32:07 -0500

    Simplify build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
 })
