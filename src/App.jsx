3d33a405951c0d271ec21e28a8b176362ea462d2
jbaileystudio <90975886+jbaileystudio@users.noreply.github.com>
2025-01-27 19:12:15 -0500

    Add debug logging and background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import InteractiveDrawing from './components/InteractiveDrawing'
-
 function App() {
+  console.log('App is rendering');
   return (
-    <div style={{ width: '100vw', height: '100vh', position: 'absolute', top: 0, left: 0 }}>
+    <div style={{ width: '100vw', height: '100vh', position: 'absolute', top: 0, left: 0, background: '#f0f0f0' }}>
       <InteractiveDrawing />
     </div>
   )
 }
-
 export default App
