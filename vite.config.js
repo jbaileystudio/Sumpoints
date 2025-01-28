75cbe32926a8be7bf7797becdf7da61779f9a509
jbaileystudio <90975886+jbaileystudio@users.noreply.github.com>
2025-01-28 12:06:08 -0500

    Updated vyte, root index.html, verified .yml

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: '/Sumpoints/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
-      },
-    },
-  },
+  }
 })
