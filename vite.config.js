69bd2509501cccfd42617be5bc2acbcb5af9859c
jbaileystudio <90975886+jbaileystudio@users.noreply.github.com>
2025-01-28 07:09:55 -0500

    Fix module loading and build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  root: './',
   base: '/',
   build: {
     outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
   },
 })
